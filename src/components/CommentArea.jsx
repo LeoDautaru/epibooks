95cfad4d037a116cc938fe92b67359a1b0585cc5
LeoDautaru <leutzu_awwr@yahoo.com>
2025-04-08 00:18:47 +0200

    -

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -7,31 +7,66 @@ import Error from './Error'
 class CommentArea extends Component {
   state = {
     comments: [],
-    isLoading: true,
+    isLoading: false,
     isError: false,
   }
 
-  componentDidMount = async () => {
-    try {
-      let response = await fetch(
-        'https://striveschool-api.herokuapp.com/api/comments/' +
-          this.props.asin,
-        {
-          headers: {
-            Authorization: 'Bearer inserisci-qui-il-tuo-token',
-          },
+  // componentDidMount = async () => {
+  //   try {
+  //     let response = await fetch(
+  //       'https://striveschool-api.herokuapp.com/api/comments/' +
+  //         this.props.asin,
+  //       {
+  //         headers: {
+  //           Authorization:
+  //             'Bearer inserisci-qui-il-tuo-token',
+  //         },
+  //       }
+  //     )
+  //     console.log(response)
+  //     if (response.ok) {
+  //       let comments = await response.json()
+  //       this.setState({ comments: comments, isLoading: false, isError: false })
+  //     } else {
+  //       console.log('error')
+  //       this.setState({ isLoading: false, isError: true })
+  //     }
+  //   } catch (error) {
+  //     console.log(error)
+  //     this.setState({ isLoading: false, isError: true })
+  //   }
+  // }
+
+  componentDidUpdate = async (prevProps) => {
+    if (prevProps.asin !== this.props.asin) {
+      this.setState({
+        isLoading: true,
+      })
+      try {
+        let response = await fetch(
+          'https://striveschool-api.herokuapp.com/api/comments/' +
+            this.props.asin,
+          {
+            headers: {
+              Authorization: 'Bearer inserisci-qui-il-tuo-token',
+            },
+          }
+        )
+        console.log(response)
+        if (response.ok) {
+          let comments = await response.json()
+          this.setState({
+            comments: comments,
+            isLoading: false,
+            isError: false,
+          })
+        } else {
+          this.setState({ isLoading: false, isError: true })
         }
-      )
-      console.log(response)
-      if (response.ok) {
-        let comments = await response.json()
-        this.setState({ comments: comments, isLoading: false, isError: false })
-      } else {
+      } catch (error) {
+        console.log(error)
         this.setState({ isLoading: false, isError: true })
       }
-    } catch (error) {
-      console.log(error)
-      this.setState({ isLoading: false, isError: true })
     }
   }
 
